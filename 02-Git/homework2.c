ef5c9f339ff0d91ea34da100fc83569c2c3eb888
Andrii Baliuk <zerksis.break@gmail.com>
2019-05-26 17:45:52 +0300

    02-Git: Implement user interface
    Signed-off-by: Andrii Baliuk <zerksis.break@gmail.com>

--- 02-Git/homework2.c
+++ 02-Git/homework2.c
@@ -1,6 +1,29 @@
 #include "stdio.h"
+#include "stdlib.h"
+#include "time.h"
+
+char *choiceConvertor(char gameChoice)
+{
+  if (gameChoice == 'r')
+    return "rock";
+  if (gameChoice == 's')
+    return "scissors";
+  if (gameChoice == 'p')
+    return "paper";
+}
+
+char readUser()
+{
+  printf("Please write one of: rock (r) - paper (p) - scissors (s)\n");
+  char user;
+  while (user != 'r' && user != 'p' && user != 's')
+    scanf("%c", &user);
+  return user;
+}
 
 int main(void)
 {
-    return 0;
+  char userChoice = readUser();
+  printf("User choice is: %s\n", choiceConvertor(userChoice));
+  return 0;
 }
