bd84c940de4e3ad038fa564f99c8926e60d6b375
Pavel Samko <bulldozerbsg@gmail.com>
2019-05-26 18:04:52 +0300

    02-Homework: Add request and processing of the answer from the user
    Added initial game conditions
    
    Signed-off-by: Pavel Samko <bulldozerbsg@gmail.com>

--- 02-Git/src/main.c
+++ 02-Git/src/main.c
@@ -1,4 +1,22 @@
+#include <stdio.h>
+
 int main()
 {
+    char buff[100];
+    int n;
+
+    printf("I made a number from 0 to 10. Try to guess.\n");
+    for (;;)
+    {
+        printf("Please enter you number: ");
+        const char *s = fgets(buff, sizeof(buff), stdin);
+        if (!s || sscanf(s, "%d", &n) != 1)
+        {
+            printf("Not a number\n");
+            continue;
+        }
+
+        printf("Entered number: %d\n", n);
+    }
     return 0;
 }
