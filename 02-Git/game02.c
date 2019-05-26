27f9e76bdbaea4fae4e843dac2021b5d6ce75fee
SanB <sankharkov@gmail.com>
2019-05-26 23:18:07 +0300

    02-Git: Implement the user number input and the stub of the number generating funtion
    Signed-off-by: SanB <sankharkov@gmail.com>

--- 02-Git/game02.c
+++ 02-Git/game02.c
@@ -3,11 +3,42 @@
 #define RET_OK 	0
 #define RET_ERR 1
 
+#define RANGE_MIN 1
+#define RANGE_MAX 10
+
+int getProgNum(int , int );
+
 int main()
 {
+	int progNum;
+	int userNum;
+	int ret;
+
 	printf("Hello! It is \"Game02\"!\n");
+
+	//Generate the random value in the range:
+	progNum = getProgNum(RANGE_MIN, RANGE_MAX);
+
+	printf("I am thinking of a number between %d and %d. Can you guess what is this number?\n", RANGE_MIN, RANGE_MAX);
+	printf(" [%d..%d]: ", RANGE_MIN, RANGE_MAX);
+	ret = scanf("%d", &userNum);
+	if (ret != 1 || userNum < RANGE_MIN || userNum > RANGE_MAX)
+	{
+		printf("Incorrect input is detected! Exit.\n");
+		return RET_ERR;
+	}
+
+	//Here check the input value and print the game result:
 
 	return RET_OK;
 }
+
+//Returns a random value in the range from 'valFrom' to 'valTo'.
+int getProgNum(int valFrom, int valTo)
+{
+	return 0;
+}
+
+
 
 
