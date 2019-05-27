baeb2a195773dde88e9ad188c64e9de8c2b05f7c
Oleksandr Siruk <alexsirook@gmail.com>
2019-05-27 12:10:50 +0300

    02-git Implement random number generator
    Signed-off-by: Oleksandr Siruk <alexsirook@gmail.com>

--- 02-Git/rps.c
+++ 02-Git/rps.c
@@ -1,6 +1,16 @@
 #include "stdio.h"
+#include "stdlib.h"
+#include "time.h"
 
 int main(void)
 {
+	int b = 0;
+	time_t t;
+
+	//Intializes random number generator
+	srand((unsigned) time(&t));
+	b = rand() % 3; // will return value from 0 to 2
+	printf("rand value = %d\n", b);
+
 	return 0;
 }
