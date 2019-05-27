37345ff28e591b6a718f6cd014e1b3f83dcb363e
Maksym Glubokiy <M.Glubokiy@gmail.com>
2019-05-27 19:53:54 +0300

    color-quiz: Implement color-quiz
    Based on rand().
    
    While at it, add clean target into makefile
    
    Signed-off-by: Maksym Glubokiy <M.Glubokiy@gmail.com>

--- 02-Git/maksym.glubokiy/color-quiz.c
+++ 02-Git/maksym.glubokiy/color-quiz.c
@@ -1,4 +1,38 @@
-int main(void)
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+static const char *colors[] = {
+	"Red",
+	"Orange",
+	"Yellow",
+	"Green",
+	"Light-Blue",
+	"Blue",
+	"Purple",
+	"",
+	NULL,
+};
+
+static unsigned get_colors_size(const char *colors[])
 {
+	unsigned rv = 0;
+
+	for (const char **p = colors; *p; p++, rv++);
+
+	return rv;
+}
+
+int main(int argc, char *argv[])
+{
+	unsigned color_idx = rand() % get_colors_size(colors);
+	char *answer = "";
+	const char *guess = colors[color_idx];
+
+	if (argc > 1) {
+		answer = argv[1];
+	}
+
+	printf("%s\r\n", !strcmp(answer, guess) ? "Bingo" : "Missed");
 	return 0;
 }
