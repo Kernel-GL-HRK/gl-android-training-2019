3ecf00ed78b095a801e9af69ace6b1ec43a6a00e
Leonid Gonchar <goncharleonid@gmail.com>
2019-05-31 17:01:06 +0300

    02-Git: Add basic logic
        - Checking basic logic, using hardcoded value;
        - Using cycle to be able add multiple iterations;
    
    Signed-off-by: Leonid Gonchar <goncharleonid@gmail.com>

--- 02-Git/homework-02.cpp
+++ 02-Git/homework-02.cpp
@@ -2,5 +2,30 @@
 using namespace std;
 
 int main(int argc, char **argv) {
+    bool exit = false;
+    bool result = false;
+
+    cout << "Lets play the game!!!" << endl;
+
+    while (false == exit) {
+        char number = '0';
+
+        cout << "Guess a number from 0 to 9" << endl;
+        cin >> number;
+
+        if ('0' == number) {
+            result = true;
+        }
+
+        cin.clear();
+        exit = true;
+    }
+
+    if (result) {
+        cout << "You've won!!!" << endl;
+    } else {
+        cout << "You've  lose!!!" << endl;
+    }
+
     return 0;
 }
