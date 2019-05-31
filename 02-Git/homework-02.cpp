8d5c91ead19f145ea78248a3eaf8e902a027b8ce
Leonid Gonchar <goncharleonid@gmail.com>
2019-05-31 17:15:48 +0300

    02-Git: Implement multiple attempts
        - Attempts number is hardcoded to 3
    
    Signed-off-by: Leonid Gonchar <goncharleonid@gmail.com>

--- 02-Git/homework-02.cpp
+++ 02-Git/homework-02.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 
+#define MAX_ATTEMPTS_NUMBER 3
+
 int main(int argc, char **argv) {
     bool exit = false;
     bool result = false;
@@ -9,16 +11,25 @@ int main(int argc, char **argv) {
 
     while (false == exit) {
         char number = '0';
+        static int attempts = MAX_ATTEMPTS_NUMBER;
 
         cout << "Guess a number from 0 to 9" << endl;
         cin >> number;
 
+        attempts--;
+
+        /* Checking result here */
         if ('0' == number) {
             result = true;
+            exit = true;
+        } else if (attempts > 0) {
+            cout << "You have " << attempts << " more attempts. Try to guess." << endl;
+        } else {
+            exit = true;
         }
 
+        /* Clear cin to truncate all characters except first one */
         cin.clear();
-        exit = true;
     }
 
     if (result) {
