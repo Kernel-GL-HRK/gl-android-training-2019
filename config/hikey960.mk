81feb8fab7bf01a5da601d6107adaa752bf5a6aa
Andrii Baliuk <zerksis.break@gmail.com>
2019-07-21 22:10:35 +0300

    LedHal: Add app and service build, on early-boot
    Signed-off-by: Andrii Baliuk <zerksis.break@gmail.com>

--- config/hikey960.mk
+++ config/hikey960.mk
@@ -2,3 +2,14 @@
 
 PRODUCT_PACKAGES +=\
 	vendor.bksnake.smartledcontroller@1.0-service.hikey960\
+	LedControllerService\
+	LedController\
+
+DEVICE_MANIFEST_FILE += \
+    vendor/bksnake/config/manifest.xml
+
+DEVICE_MATRIX_FILE += \
+    vendor/bksnake/config/compatibility_matrix.xml
+
+BOARD_SEPOLICY_DIRS += \
+    vendor/bksnake/sepolicy
